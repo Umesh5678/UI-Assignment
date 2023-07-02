8cc1809161657f67c1385cc0c4dcdbc498919625
Umesh Rajendra Mahajan <126227089+Umesh5678@users.noreply.github.com>
2023-07-02 17:24:45 +0530

    Add files via upload

--- script1.js
+++ script1.js
@@ -2,18 +2,66 @@ function Umesh(){
     var x = localStorage.getItem('data');
     var obj = JSON.parse(x);
     var name = $("#search").val();
+    
     $(".container1").empty();
     $.each(obj, function( index, value ) {
         if(name == obj[index]['Fname'])
         {
+            var fname= value['Fname'];
+            $(".start").append(fname[0]);
+            $(".start").css(
+                
+                    {
+                      "position": "absolute",
+                      "font-size": "50px",
+                      "font-weight": "700",
+                      "margin-left": "20px"
+                    }
+            )
+            $("#box_div").css(
+                
+                {
+                    "height": "80px",
+                    "width": "80px",
+                    "background-color": "orange",
+                    "float": "center",
+                    "border-radius": "50%",
+                    "position":"relative"
+                    }
+            )
             $(".fname").append(value['Fname']);
             $(".lname").append(value['Lname']);
             $(".email").append(value['Email']);
             $(".address").append(value['Address']);
-            $(".topic_name").append(value['Topic_name']);
+            $(".contact").append(value['Contact']);
+
+            $(".city").append(value['City']+",");
+            $(".state").append(value['State']+",");
+            $(".country").append(value['Country']+",");
+            
+            
+            var specialization = value['Subject_name'];
+            specialization.forEach(element => {
+                
+                $(".specialization").append(element+"<br>");
+  
+            });
+            
+            
+            var topic_name = value['Topic_name'];
+            topic_name.forEach(element => {
+                $(".topic_name").append(element+"<br>");
+                
+                
+            });
+
+   
+            
         }
             
     });
+
+    
 
     $(".export").click(function(){
         const fileBlob = new Blob([x], { type: 'application/json' });
@@ -26,6 +74,5 @@ function Umesh(){
     });
 
 }
-
 
 
